7d626523c1414e354ef364d6f66e122597ba728f
Saroj Patro <sarojkumar.patro@siemens.com>
2023-12-12 23:58:44 +0000

    Code challenge submission for question: 1464 Maximum Product of Two Elements in an Array

---
+++ 1464 Maximum Product of Two Elements in an Array/1464maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max1 = -1, max2 = -1;
+        for (auto num : nums)
+        {
+            if(num > max1)
+            {
+                max2 = max1;
+                max1 = num;
+            }
+            else if (num > max2)
+            {
+                max2 = num;
+            }
+        }
+
+        return (max1 -1)*(max2 -1);
+    }
+};
