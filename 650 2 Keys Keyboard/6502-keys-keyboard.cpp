a4649ad80c17621e3eac409e9eef583018ce78c4
Saroj Patro <sarojkumar.patro@siemens.com>
2024-08-19 06:07:39 +0000

    Code challenge submission for question: 650 2 Keys Keyboard

---
+++ 650 2 Keys Keyboard/6502-keys-keyboard.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        vector<int> dp(n + 1);
+        for(int i = 2; i<n+1; i++) dp[i] = i;
+        for(int i = 2; i <= n; i++)
+            for(int j = 2; j < i; j++)
+               if(i%j == 0) dp[i] = min(dp[i], dp[i/j] + j);
+        return dp[n];
+    }
+};
